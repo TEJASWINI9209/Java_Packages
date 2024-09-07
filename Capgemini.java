8bafea9ed1a61b25667e17b525bafbea163d5701
TEJASWINI <annadatetejaswini@gmail.com>
2024-09-07 21:36:00 +0530

    Example on package creation

---
+++ Capgemini.java
@@ -0,0 +1,12 @@
+package com.capgemini.hiring;
+import biz.cybersuccess.it.training.CyberSuccess;
+class Capgemini extends CyberSuccess 
+{
+	public static void main(String[] args) 
+	{
+		Capgemini cp=new Capgemini();
+		cp.testing();
+		cp.java();
+		cp.python();
+	}
+}
---
+++ CyberSuccess.java
@@ -0,0 +1,14 @@
+package biz.cybersuccess.it.training;
+public class CyberSuccess
+{
+	protected void testing(){
+		System.out.println("Candidate for testing");
+	}
+	protected void java(){
+		System.out.println("Candidate for java");
+	}
+	protected void python(){
+		System.out.println("Candidate for python");
+	}
+}
+
